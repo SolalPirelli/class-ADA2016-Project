b294641d69693d3de4ba3b01064eb973f809f8ee
tharvik <tharvik@users.noreply.github.com>
2017-01-05 13:27:54 +0100

    goes from user friendly rts to dump

--- lib.py
+++ lib.py
@@ -1,14 +1,12 @@
 import datetime
 import requests
 from collections import namedtuple
+from bs4 import BeautifulSoup as BeautifulSoup
+
+SEP = '\x1F'
+bs = lambda t: BeautifulSoup(t, 'lxml')
 
 Title = namedtuple('Title', ['title', 'artist', 'date'])
-
-class EmissionTitle:
-    def __init__(self, title, album, artist, chapter):
-        self.title = title
-        self.artist = artist
-        self.date = date
 
 def dates_from_today():
     old = datetime.date.today()
@@ -26,6 +24,16 @@ def get(url, **kwargs):
 
     return req
 
+def post(url, **kwargs):
+    req = session.post(url, **kwargs)
+    assert req.status_code == 200
+
+    return req
+
 def print_titles(titles):
     for t in titles:
         print(t.date, t.title, t.artist)
+
+def dump_titles(titles):
+    for t in titles:
+        print('{}{}{}{}{}'.format(t.date, SEP, t.title, SEP, t.artist))
